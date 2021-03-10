b603c95d6bd8da6f93ccd0c56f14d037d83225d6
jiangzhihong <woyaojiayou_a@163.com>
2021-03-10 12:59:31 +0800

    增加异常处理

--- src/main/java/com/and/exception/GlobalExceptionHandler.java
+++ src/main/java/com/and/exception/GlobalExceptionHandler.java
@@ -12,8 +12,6 @@ import javax.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-	private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
