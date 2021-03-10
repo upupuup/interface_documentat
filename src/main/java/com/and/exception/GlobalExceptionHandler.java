3940603ab10ef403466712f2493954858e3a67db
jiangzhihong <woyaojiayou_a@163.com>
2021-03-10 12:59:18 +0800

    增加异常处理

--- src/main/java/com/and/exception/GlobalExceptionHandler.java
+++ src/main/java/com/and/exception/GlobalExceptionHandler.java
@@ -9,13 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
-import java.sql.SQLException;
 
-/**
- *  @Description: 全局异常处理如果使用@RestControllerAdvice 注解则会将返回的数据类型转换成JSON格式
- *  @author: miaojy
- *  @date: 2020-10-15 15:35:57
- */
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 	private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
--- src/main/java/com/and/utils/BaseResponseUtils.java
+++ src/main/java/com/and/utils/BaseResponseUtils.java
@@ -3,10 +3,6 @@ package com.and.utils;
 import com.and.response.BaseResponse;
 import java.util.Map;
 
-/**
- * 获取一个公共返回的类
- * @author: miaojinyong
- */
 public class BaseResponseUtils {
     public static BaseResponse getBaseResponse(String success, String tipMsg, Map<String, Object> data){
     	return new BaseResponse(success, tipMsg, data);
